8cdfdc4e90bfa13f0f6d09df9fd1549ef8a8fce6
123andrzej123 <krzysztof.bulawski@gmail.com>
2017-12-26 12:54:45 +0100

    init

---
+++ app/src/main/java/com/example/a123andrzej123/taxi/TaxiActivity.java
@@ -0,0 +1,13 @@
+package com.example.a123andrzej123.taxi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TaxiActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taxi);
+    }
+}
